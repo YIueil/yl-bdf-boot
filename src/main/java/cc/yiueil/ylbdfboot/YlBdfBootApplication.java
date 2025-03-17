09a4764320677a3b0a1262fb60d4f936263b46c3
YIueil <YIueil@163.com>
2025-03-17 11:34:03 +0800

    init: 项目初始化

---
+++ src/main/java/cc/yiueil/ylbdfboot/YlBdfBootApplication.java
@@ -0,0 +1,13 @@
+package cc.yiueil.ylbdfboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YlBdfBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YlBdfBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cc/yiueil/ylbdfboot/YlBdfBootApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.yiueil.ylbdfboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YlBdfBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
