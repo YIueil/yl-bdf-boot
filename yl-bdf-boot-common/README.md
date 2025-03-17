7504f1a0c69a089a28611d99da512e1ecae94eec
YIueil <YIueil@163.com>
2025-03-17 11:54:11 +0800

    init: 项目初始化

---
+++ yl-bdf-boot-common/README.md
@@ -0,0 +1,2 @@
+# module description
+common 模块，存放公共的一些组件
--- yl-bdf-boot-start/src/main/java/cc/yiueil/starter/YlBdfBootApplication.java
+++ yl-bdf-boot-start/src/main/java/cc/yiueil/starter/YlBdfBootApplication.java
@@ -1,4 +1,4 @@
-package cc.yiueil.ylbdfboot;
+package cc.yiueil.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
